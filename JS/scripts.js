a0062991ccadaa1741dfa2d506f89146ff4be045
Amir <aamirvand97@gmail.com>
2020-05-21 10:21:04 -0700

    updated a few things before initial release

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+//copy and paste funcction for contact button
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
